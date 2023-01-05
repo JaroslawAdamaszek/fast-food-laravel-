5599994b37bcdd289321e3b0c5b1d5033ddfef1c
maasiek <masiu.veb@gmail.com>
2023-01-05 16:52:13 +0100

    creating test user & fix password hashing

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UsersSeeder extends Seeder
 {
@@ -14,6 +16,12 @@ class UsersSeeder extends Seeder
      */
     public function run()
     {
-        //
+        User::create([
+            "name" => 'Foodie',
+            "email" => 'foodie@fast-food.store',
+            "address" => 'Smakoszowo',
+            "password" => Hash::make('Tv3SH6a0OphS'),
+            "phone" => "111222333",
+        ]);
     }
 }
