30e62becc0697c31c0bb188aa079d3553e3455c2
maasiek <masiu.veb@gmail.com>
2023-01-06 18:10:20 +0100

    changes in the display of the validation message

--- resources/views/product/edit.blade.php
+++ resources/views/product/edit.blade.php
@@ -2,15 +2,7 @@
 
 
 @section('content')
-    @if ($errors->any())
-        <div class="alert alert-danger m-auto w-50 mb-2">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
+
     <div class="container">
         <div class="row row-centered pos">
             <div class=" card col-lg-6 col-sm-12 m-auto">
@@ -35,13 +27,18 @@
                             @foreach(\App\Http\Controllers\IngredientsController::index($product['name']) as $type => $ingredients)
                                 <div class="mt-2"><span class="badge bg-warning text-dark mb-2">{{$type}}</span></div>
                                 @foreach($ingredients as $ingredient)
-                                    <input type="checkbox" class="btn-check" id="btn-check-{{$ingredient['name']}}"
+                                    <input type="checkbox" class="btn-check form-control @error($type) is-invalid @enderror" id="btn-check-{{$ingredient['name']}}"
                                            name="{{$type}}[]"
                                            value="{{$ingredient['name']}}" autocomplete="off"
                                         {{in_array($ingredient['name'], $product[$type]) ? 'checked': ''}}>
                                     <label class="btn btn-outline-secondary mt-1 ms-1"
                                            for="btn-check-{{$ingredient['name']}}">{{$ingredient['name']}}</label>
                                 @endforeach
+                                @error($type)
+                                <span class="invalid-feedback" role="alert">
+                                              <strong>{{ $message }}</strong>
+                                              </span>
+                                @enderror
                             @endforeach
                             <div class="container d-flex align-items-center justify-content-center mt-3">
                                 <button type="submit" class="btn btn-primary">Save</button>
